01e819ca4b8d8500c5085fef3be0429487d0d965
Débora-Karine-Cassimiro <61765035+deboraKa@users.noreply.github.com>
2024-10-29 15:38:36 -0300

    atividade-proz
    Crie um novo projeto com dois arquivos: index.html e script.js. No arquivo HTML, defina a estrutura padrão HTML, e inclua uma tag h1 com o título "Conexão com um arquivo JavaScript". No arquivo JavaScript defina uma função que imprima a mensagem "Conexão feita com sucesso!" no terminal, e execute ela três vezes. Finalmente, conecte ambos arquivos, abra seu projeto no navegador usando a extensão LiveServer e confira se a mensagem foi impressa três vezes no terminal.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função que imprime a mensagem no console
+function exibirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Executando a função três vezes
+exibirMensagem();
+exibirMensagem();
+exibirMensagem();
